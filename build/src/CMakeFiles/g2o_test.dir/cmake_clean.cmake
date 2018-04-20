33c6bfa0bbfe55a5316b5de916c7fa1e14852e32
WangYong <1459802323@qq.com>
2018-04-20 14:32:40 +0800

    after build and run

---
+++ build/src/CMakeFiles/g2o_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/g2o_test.dir/main.cpp.o"
+  "../../bin/g2o_test.pdb"
+  "../../bin/g2o_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/g2o_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
